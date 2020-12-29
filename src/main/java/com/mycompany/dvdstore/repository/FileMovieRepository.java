5bb0287272caf45d22e67f22b844b241d07a13bd
goncalvesjp <goncalvesjp.dev@pm.me>
2020-12-29 16:19:25 +0100

    tp7

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,20 +2,28 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileMovieRepository implements MovieRepositoryInterface{
 
+    private File file;
+
     public void add(Movie movie) {
         FileWriter writer;
         try {
-            writer = new FileWriter("movies.txt", true);
+            writer = new FileWriter(file, true);
+            //writer = new FileWriter("movies.csv", true);
             writer.write(movie.getTitle()
                     + ";" + movie.getGenre() + "\n");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 }
