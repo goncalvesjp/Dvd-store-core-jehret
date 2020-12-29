ac644a06135afae33bc6575f7c4f8af6ca0b780a
goncalvesjp <goncalvesjp.dev@pm.me>
2020-12-29 18:49:54 +0100

    tp11

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,8 +1,6 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.FileMovieRepository;
-import com.mycompany.dvdstore.service.DefaultMovieService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,13 +13,6 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-//        FileMovieRepository movieRepository = new FileMovieRepository();
-//        DefaultMovieService defaultMovieService = new DefaultMovieService();
-//        defaultMovieService.setMovieRepository(movieRepository);
-//
-//        MovieController movieController = new MovieController();
-//        movieController.setMovieService(defaultMovieService);
-//        movieController.addUsingConsole();
 
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
