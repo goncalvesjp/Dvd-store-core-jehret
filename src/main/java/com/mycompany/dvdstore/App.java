47a2ae0f452e6254d5aabc2e9f4ff001c02d1321
goncalvesjp <goncalvesjp.dev@pm.me>
2020-12-30 00:04:16 +0100

    tp12

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,7 +2,7 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -12,8 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
+        //ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         MovieController movieController = context.getBean(MovieController.class);
         movieController.addUsingConsole();
 
