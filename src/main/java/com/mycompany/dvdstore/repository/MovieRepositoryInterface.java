bb896723a4d86766fa041731c4d1622feb3d50db
goncalvesjp <goncalvesjp.dev@pm.me>
2021-02-22 17:07:43 +0100

    tp31

--- src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
+++ src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
@@ -3,8 +3,6 @@ package com.mycompany.dvdstore.repository;
 import com.mycompany.dvdstore.entity.Movie;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface MovieRepositoryInterface /*extends CrudRepository<Movie,Long>*/ {
     Movie add(Movie movie);
     List<Movie> list();
--- src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -1,6 +1,5 @@
 package com.mycompany.dvdstore.service;
 
-
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,26 +8,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class DefaultMovieService implements MovieServiceInterface {
 
-
+    @Autowired
     private MovieRepositoryInterface movieRepository;
 
-    @Autowired
+    public MovieRepositoryInterface getMovieRepository() {
+        return movieRepository;
+    }
+
     public void setMovieRepository(MovieRepositoryInterface movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-
     public Movie registerMovie(Movie movie) {
-        return movieRepository.add(movie);
+        return movieRepository.save(movie);
     }
 
     @Override
     public Iterable<Movie> getMovieList() {
-        return movieRepository.list();
+        return movieRepository.findAll();
     }
 
     @Override
     public Movie getMovieById(long id) {
-        return movieRepository.getById(id);
+        return movieRepository.findById(id).orElseThrow();
     }
 }
